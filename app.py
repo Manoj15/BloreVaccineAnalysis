3862aa3f7e596f839a68f7888402806572eeffb2
Manoj Kumar <thecurvefitter@gmail.com>
2021-05-29 01:25:38 +0530

    working app

---
+++ app.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import streamlit as st
+from copy import deepcopy
+import matplotlib as mpl
+mpl.use("agg")
+from matplotlib.backends.backend_agg import RendererAgg
+_lock = RendererAgg.lock
+import plotly.express as px
+
+import fetch_tweet
+
+
+# -- Set page config
+apptitle = 'Bangalore COVID-19 Vaccines Slot Availability Analysis'
+
+st.set_page_config(page_title=apptitle, page_icon=":eyeglasses:")
+
+# -- Default detector list
+detectorlist = ['H1','L1', 'V1']
+
+# Title the app
+st.title('Bangalore COVID-19 Vaccines Slot Availability Analysis')
+
+
+# Fetch Data from Twitter
+# st.set_page_config(page_title=None, page_icon=None, layout='centered', initial_sidebar_state='auto')
+@st.cache
+def fetch_data():
+    # This function will only be run the first time it's called
+    fetch_tweet.fetch_data()
+
+fetch_data()
+
+
+# Date Read
+tweets_df2 = pd.read_csv('app_data.csv')
+tweets_df2['Datetime'] = pd.to_datetime(tweets_df2['Datetime'])
+
+st.sidebar.markdown("## Data Filter")
+
+#-- Select Hospital
+hospitals = tweets_df2.groupby('Hospital_Name').Capacity.count().reset_index()
+hospitals = hospitals.sort_values('Capacity', ascending = False)
+select_hospital = st.sidebar.selectbox('Select Hospital to filter',
+                                hospitals.Hospital_Name.unique())
+
+# Time Series Plot for entire city
+st.header('Slot Availability Till Date for Bangalore')
+plot_df_1 = deepcopy(tweets_df2)
+plot_df_1['Date'] = plot_df_1.Datetime.dt.date
+plot_df_1 = plot_df_1.groupby('Date').Capacity.sum().reset_index()
+fig1 = px.line(plot_df_1, x='Date', y="Capacity")
+st.plotly_chart(fig1, clear_figure=True)
+
+# Tree plot of hospital/zip code slot availability
+plot_df_2 = tweets_df2.groupby(['zip_code', 'Hospital_Name']).Capacity.sum().reset_index()
+fig2 = px.treemap(plot_df_2, path=['zip_code', 'Hospital_Name'], values='Capacity')
+st.plotly_chart(fig2, clear_figure=True)
+
+# Time Series plot of hospital wise 
+st.header('Slot Availability Till Date for Hospital Selected')
+plot_df_3 = tweets_df2[tweets_df2.Hospital_Name == select_hospital]
+plot_df_3['Date'] = plot_df_3.Datetime.dt.date
+plot_df_3 = plot_df_3.groupby('Date').Capacity.sum().reset_index()
+fig3 = px.line(plot_df_3, x='Date', y="Capacity")
+st.plotly_chart(fig3, clear_figure=True)
+
+# Day of Week plot of Hospital wise
+plot_df_4 = tweets_df2[tweets_df2.Hospital_Name == select_hospital]
+plot_df_4['DayName'] = plot_df_4.Datetime.dt.day_name()
+plot_df_4 = plot_df_4.groupby('DayName').Capacity.sum().reset_index()
+fig4 = px.bar(plot_df_4, x='DayName', y='Capacity', category_orders={'DayName':['Monday','Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']})
+st.plotly_chart(fig4, clear_figure=True)
+
+# Time of Day of Hospital Wise
+plot_df_5 = tweets_df2[tweets_df2.Hospital_Name == select_hospital]
+plot_df_5['HourofDay'] = plot_df_5['Hour']
+plot_df_5 = plot_df_5.groupby('HourofDay').Capacity.sum().reset_index()
+fig5 = px.bar(plot_df_5, x='HourofDay', y='Capacity', category_orders={'DayName':[str(i) for i in range(0, 25)]})
+st.plotly_chart(fig5, clear_figure=True)
+
+st.subheader('Please Note : Just for Educational Purposes. Use with Caution')
---
+++ fetch_tweet.py
@@ -0,0 +1,71 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+import re
+from datetime import datetime
+from dateutil import tz
+import pytz
+
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+def get_IST(date):
+    
+    gmt = pytz.timezone('GMT')
+    eastern = tz.tzlocal()
+    date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S+00:00')
+
+    dategmt = gmt.localize(date)
+    date_ist = dategmt.astimezone(eastern)
+    return str(date_ist)
+
+def preprocess(df):
+    
+    # Filter only tweets containing vaccine info by using keywords like "available for 18 to 44" as the user only tweeets for that
+    df = df[df.Text.str.contains("available for 18 to 44")]
+    
+    # Date
+    df['Datetime']  =  df['Datetime'].apply(lambda x : get_IST(str(x)))
+    df['Datetime'] = pd.to_datetime(df['Datetime'], format='%Y-%m-%d %H:%M:%S')
+    
+    # Vaccine (looks like this field started after sometime)
+    df['Vaccine'] = df.Text.apply(lambda x : 'COVAXIN' if 'COVAXIN' in x else ('COVISHIELD' if 'COVISHIELD' in x else ''))
+    df = df[df.Vaccine.notna()]
+    
+    # Zip Code
+    df['zip_code'] = df.Text.apply(lambda x : re.match('\d{6}', x).group(0))
+    
+    # Hospital Name
+    df['Hospital_Name'] = df.Text.apply(lambda x :  re.search('at (.+?)\(', x).group(0).replace('at ', '').replace('(', ''))
+    df['Hospital_Name'] = df.Hospital_Name.apply(lambda x : x.split('on')[0][:-1])
+    
+     # Capacity(WIP)
+    # df['Capacity'] = df.Text.apply(lambda x :   re.search('Capacity: (.+?),', x).group(0).replace(',', '').replace('Capacity: ',''))
+    df['Capacity'] = df.apply(lambda x: re.findall(r'\d+', (x.Text.split(x['Hospital_Name'])[1]).split('(')[1]) if x['Hospital_Name'] != '' else x, axis = 1)
+    df = df[df.Hospital_Name != '']
+    df['Capacity'] = df.Capacity.apply(lambda x: x[0] if len(x) > 0 else -999)
+    df = df[df.Capacity != -999]
+    
+    # Get time attributes
+    df['Hour'] = df['Datetime'].dt.hour
+    df['Day'] = df['Datetime'].dt.day
+    df['dayofweek'] = df['Datetime'].dt.dayofweek
+    
+    df = df.reset_index(drop = True)
+
+    return df
+
+def fetch_data():
+    # Creating list to append tweet data
+    tweets_list1 = []
+
+    # Using TwitterSearchScraper to scrape data and append tweets to list
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:BloreVaccine' +  ' lang:en' + ' -filter:links -filter:replies').get_items()):
+        tweets_list1.append([tweet.date, tweet.id, tweet.content, tweet.username])
+        
+    # Creating a dataframe from the tweets list above 
+    tweets_df1 = pd.DataFrame(tweets_list1, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+
+    tweets_df2 = preprocess(tweets_df1)
+
+    tweets_df2.to_csv('app_data.csv', index = False)
